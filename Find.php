e4f2cb1553c4aa631efd5e92ccae1a5015617025
KunalVichave-CP <93924270+KunalVichave-CP@users.noreply.github.com>
2022-03-17 10:07:03 +0530

    Add files via upload

---
+++ Find.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="INTUITIVE">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Find</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Find.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.6.5, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/logo1.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Find">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body u-xl-mode"><header class="u-clearfix u-grey-5 u-header u-header" id="sec-576a"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="#" class="u-image u-logo u-image-1" data-image-width="427" data-image-height="222" title=".">
+          <img src="images/logo1.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Home.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="About.html" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Contact.html" style="padding: 10px 20px;">Contact</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Find.html" style="padding: 10px 20px;">Find</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Home.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="About.html" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Contact.html" style="padding: 10px 20px;">Contact</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Find.html" style="padding: 10px 20px;">Find</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-clearfix u-section-1" id="sec-8d59">
+      <div class="u-clearfix u-sheet u-sheet-1">
+
+        <!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Select Dropdown Example in PHP</title>
+  <style>
+    .container {
+      max-width: 350px;
+      margin: 50px auto;
+      text-align: center;
+    }
+    input[type="submit"] {
+      margin-bottom: 20px;
+    }
+    .select-block {
+      width: 300px;
+      margin: 110px auto 30px;
+      position: relative;
+    }
+    select {
+      width: 100%;
+      height: 50px;
+      font-size: 100%;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 0;
+      background-color: #333333;
+      border: none;
+      border: 2px solid #111111;
+      border-radius: 10px;
+      color: white;
+      appearance: none;
+      padding: 8px 38px 10px 18px;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      transition: color 0.3s ease, background-color 0.3s ease, border-bottom-color 0.3s ease;
+    }
+    /* For IE <= 11 */
+    select::-ms-expand {
+      display: none;
+    }
+    .selectIcon {
+      top: 7px;
+      right: 15px;
+      width: 30px;
+      height: 36px;
+      padding-left: 5px;
+      pointer-events: none;
+      position: absolute;
+      transition: background-color 0.3s ease, border-color 0.3s ease;
+    }
+    .selectIcon svg.icon {
+      transition: fill 0.3s ease;
+      fill: white;
+    }
+    select:hover,
+    select:focus {
+      color: #000000;
+      background-color: white;
+    }
+    select:hover~.selectIcon,
+    select:focus~.selectIcon {
+      background-color: white;
+    }
+    select:hover~.selectIcon svg.icon,
+    select:focus~.selectIcon svg.icon {
+      fill: #1A33FF;
+    }
+    /* <!-- HTML !-->
+<button class="button-62" role="button">Button 62</button> */
+
+/* CSS */
+.button-62 {
+  background: linear-gradient(to bottom right, #EF4765, #FF9A5A);
+  border: 0;
+  border-radius: 12px;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: -apple-system,system-ui,"Segoe UI",Roboto,Helvetica,Arial,sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 2.5;
+  outline: transparent;
+  padding: 0 1rem;
+  text-align: center;
+  text-decoration: none;
+  transition: box-shadow .2s ease-in-out;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+}
+
+.button-62:not([disabled]):focus {
+  box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
+}
+
+.button-62:not([disabled]):hover {
+  box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
+}
+  </style>
+</head>
+<body>
+
+<!--  -->
+
+
+  <div class="container mt-5">
+    <form action="" method="post" class="mb-3">
+      <div class="select-block">
+        <select name="Fruit" id="sel">
+          <option value="" disabled selected>Choose option</option>
+          <option value="Apple">Apple</option>
+          <option value="Banana">Banana</option>
+          <option value="Coconut">Coconut</option>
+          <option value="Blueberry">Blueberry</option>
+          <option value="Strawberry">Strawberry</option>
+        </select>
+        <div class="selectIcon">
+          <!-- <svg focusable="false" viewBox="0 0 104 128" width="25" height="35" class="icon">
+            <path d="m2e1 95a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm14 55h68v1e1h-68zm0-3e1h68v1e1h-68zm0-3e1h68v1e1h-68z"></path>
+          </svg> -->
+
+
+          <img src="arrow.png" alt="" width="25" height="35" class="icon" focusable="false" viewBox="0 0 104 128">
+        </div>
+      </div>
+      <input type="submit" name="submit" vlaue="Choose options" class="button-62" onclick="f();">
+    </form>
+    <script>
+     function f(){
+       
+        var val=document.getElementById("sel").value;
+        switch (val) {
+          case "Apple":
+            window.open("Home.html",_self);
+            break;
+        
+          default:
+            break;
+        }
+       
+     }
+    </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- <?php
+
+// if(isset($_POST['submit'])){
+//   if(!empty($_POST['Fruit'])) {
+//     $selected = $_POST['Fruit'];
+//     echo 'You have chosen: ' . $selected;
+//     switch ($selected) {
+//       $uri ='http://';
+//       $link= $uri . $_SERVER['HTTP_HOST'] . '/k/';
+//       case 'Apple':
+
+//         header("Location: '. $link .'v.php');
+//         break;
+      
+//       default:
+//         # code...
+//         break;
+//     }
+
+
+
+
+ 
+
+
+      if(isset($_POST['submit'])){
+        if(!empty($_POST['Fruit'])) {
+          $selected = $_POST['Fruit'];
+          // echo 'You have chosen: ' . $selected;
+          switch ($selected) {
+            case $selected=='Apple':
+
+
+              header("Location:v.php");
+
+              break;
+            
+            default:
+              # code...
+              break;
+          }
+        } else {
+          echo 'Please select the value.';
+        }
+      }
+    ?> -->
+  </div>
+  
+</body>
+</html>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-d86f"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Made By Team ​© Copyright 2022 OurSuggestion</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  </body>
+</html>
